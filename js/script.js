4d93d24e0fab880d1245d14bf2f278cdc8145168
francis amos <francis.amos@amalitech.org>
2024-04-10 06:46:43 +0000

    commit to main

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 
-    ScrollReveal().reveal('[data-scroll]', {
-        delay: 300,
-        distance: '50px',
-        origin: 'bottom',
-        duration: 1000,
-        reset: true
-    });
+
+ScrollReveal().reveal('[data-scroll]', {
+    delay: 300,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    reset: true
+});
+
+
+
+let closeBtn = document.getElementById('closeBtn');
+let menuBtn = document.getElementById('menuBtn');
+let show = document.getElementById("nav-links");
+
+
+menuBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+
+function openMenu(){
+    
+    show.style.right = "0";
+}
+
+
+function closeMenu(){
+    
+    show.style.right = "-200px";
+}
