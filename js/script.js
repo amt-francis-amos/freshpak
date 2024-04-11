77ee39dd5a8371aa42f60a35faf1fc318bde3bba
francis amos <francis.amos@amalitech.org>
2024-04-11 19:44:07 +0000

    commit to main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// ----SCROLL EFFECTS --------------------------------
 ScrollReveal().reveal('[data-scroll]', {
     delay: 300,
     distance: '50px',
@@ -9,7 +9,7 @@ ScrollReveal().reveal('[data-scroll]', {
 });
 
 
-
+// ---- MENU BAR----
 let closeBtn = document.getElementById('closeBtn');
 let menuBtn = document.getElementById('menuBtn');
 let show = document.getElementById("nav-links");
@@ -42,33 +42,38 @@ form.addEventListener('submit', function(e) {
   const formData = new FormData(form);
   const object = Object.fromEntries(formData);
   const json = JSON.stringify(object);
-  result.innerHTML = "Please wait..."
+  result.innerHTML = "Please wait...";
 
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = json.message;
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
-        });
+  // Extract the rating value from the formData object
+  const rating = formData.get('rating');
+
+  // Now you can include the rating value in the JSON data to be sent
+
+  fetch('https://api.web3forms.com/submit', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          },
+          body: json
+      })
+      .then(async (response) => {
+          let json = await response.json();
+          if (response.status == 200) {
+              result.innerHTML = json.message;
+          } else {
+              console.log(response);
+              result.innerHTML = json.message;
+          }
+      })
+      .catch(error => {
+          console.log(error);
+          result.innerHTML = "Something went wrong!";
+      })
+      .then(function() {
+          form.reset();
+          setTimeout(() => {
+              result.style.display = "none";
+          }, 3000);
+      });
 });
